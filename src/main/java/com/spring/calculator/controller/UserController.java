816a1d8ec003fa005edc518b59fd56bd59e47eef
EgidijusJakimovas <egidijusjakimovas@gmail.com>
2024-05-07 14:42:19 +0300

    added user registration

---
+++ src/main/java/com/spring/calculator/controller/UserController.java
@@ -0,0 +1,70 @@
+package com.spring.calculator.controller;
+
+import com.spring.calculator.model.User;
+import com.spring.calculator.service.SecurityService;
+import com.spring.calculator.service.UserService;
+import com.spring.calculator.validator.UserValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    private SecurityService securityService;
+
+    @Autowired
+    private UserValidator userValidator;
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    public UserController(@Qualifier("UserService") UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("user", new User());
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerNewUser(@ModelAttribute("user") User user, BindingResult result) {
+        userValidator.validate(user, result);
+
+        if (result.hasErrors()) {
+            return "register";
+        }
+
+        userService.save(user);
+
+        securityService.autoLogin(user.getUsername(), user.getPassword());
+
+        return "redirect:/login?registrationSuccess";
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm(Model model, String error, String logout, String registrationSuccess) {
+        if (error != null) {
+            model.addAttribute("error", "Username or password is incorrect.");
+        }
+        return "redirect:/calculator";
+    }
+}
---
+++ src/main/java/com/spring/calculator/service/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.spring.calculator.service;
+
+import com.spring.calculator.model.User;
+import com.spring.calculator.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    @Qualifier("BCryptPasswordEncoder")
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    public void save(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setRole(User.UserRole.USER);
+
+        userRepository.save(user);
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
