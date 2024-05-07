73ea7141eceda8dc7bfcdfef27ed1d539dc22a9e
EgidijusJakimovas <egidijusjakimovas@gmail.com>
2024-05-07 14:35:52 +0300

    added functionality to save calculations in the DB

---
+++ src/main/java/com/spring/calculator/config/SpringConfig.java
@@ -0,0 +1,38 @@
+package com.spring.calculator.configuration;
+
+import com.spring.calculator.repository.NumberRepository;
+import com.spring.calculator.repository.UserRepository;
+import com.spring.calculator.service.NumberService;
+import com.spring.calculator.service.NumberServiceImpl;
+import com.spring.calculator.service.UserService;
+import com.spring.calculator.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    private final NumberRepository numberRepository;
+    private final UserRepository userRepository;
+
+
+    @Autowired
+    public SpringConfig(NumberRepository numberRepository, UserRepository userRepository) {
+        this.numberRepository = numberRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Bean
+    @Qualifier("NumberService")
+    public NumberService getNumberService() {
+        return new NumberServiceImpl(numberRepository);
+    }
+
+    @Bean
+    @Qualifier("UserService")
+    public UserService getUserService() {
+        return new UserServiceImpl();
+    }
+}
