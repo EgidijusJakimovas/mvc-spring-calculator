7fb84de1392a2c19dd36433d79da73c159146f2c
EgidijusJakimovas <egidijusjakimovas@gmail.com>
2024-05-17 00:42:56 +0300

    removed unused imports

--- src/main/java/com/spring/calculator/model/User.java
+++ src/main/java/com/spring/calculator/model/User.java
@@ -1,7 +1,6 @@
 package com.spring.calculator.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
