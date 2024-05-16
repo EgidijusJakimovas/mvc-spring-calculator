76595bf60af586f78033cbdab9691a491c2dcb01
EgidijusJakimovas <egidijusjakimovas@gmail.com>
2024-05-16 10:49:57 +0300

    deleted MVC config file

--- src/main/java/com/spring/calculator/config/ServletInitializer.java
+++ src/main/java/com/spring/calculator/config/ServletInitializer.java
@@ -11,3 +11,9 @@ public class ServletInitializer extends SpringBootServletInitializer {
         return application.sources(CalculatorApplication.class);
     }
 }
+
+// TODO:
+//      1. vientisas stilius (visoms formoms bendras footeris, headeris ir menu). Menu juostoje varotojas mato prisijungimo
+//      varda ir gali atsijungti. Menu juostuoje yra nuorodos i formas (skaiciuoti, perziureti, atnaujinti)
+//      2. kai bus sutvarkyta vartotojo validacija identiskai sutvarkyti number validator
+//      5. pabandyti mvc config paliekant kontroleryje
